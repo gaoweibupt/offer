269a1617423cfe71fb1d335d219bdd231ea05429
gaowibupt <gaoweibupt@163.com>
2015-10-16 19:14:08 +0800

    test

---
+++ src/test/testTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ *@author  gaoweibupt@gmail.com	
+ * 创建时间： 2015年10月16日下午6:52:50
+ */
+public class testTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
